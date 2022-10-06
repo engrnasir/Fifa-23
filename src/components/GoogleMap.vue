e8112b35b089d29f31126c139841e896eb79f85d
Nasir Raza <iinasirraza@gmail.com>
2022-10-07 01:49:28 +0500

    map added

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,20 @@
+<template>
+    <GMapMap
+        :center="{lat: 51.093048, lng: 6.842120}"
+        :zoom="4"
+        map-type-id="terrain"
+        style="width: 100%; height: 332px"
+    >
+    </GMapMap>
+  </template>
+  
+  <script >
+  export default {
+    name: 'App',
+    data() {
+      return {
+        center: {lat: 51.093048, lng: 6.842120},
+      }
+    }
+  }
+  </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueGoogleMaps from '@fawmi/vue-google-maps'
 
 createApp(App)
+.use(VueGoogleMaps, {
+    load: {
+        // key: 'AIzaSyBm6-O-o6ZDDfGqg6Uzhkhy-nHQDoZCkKI',
+        key: 'AIzaSyBod9qcKE75PeDr5_vvGlLrz5YSD_IbuEA',
+        
+    },
+    autobindAllEvents: true,
+})
 .use(store).use(router).mount('#app')
