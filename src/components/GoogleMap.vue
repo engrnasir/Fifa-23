dc23a2864465f27e21982980dc916c54059d9920
Nasir Raza <iinasirraza@gmail.com>
2022-10-07 02:37:20 +0500

    map updated

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -5,16 +5,6 @@
         map-type-id="terrain"
         style="width: 100%; height: 332px"
     >
-    <GMapCluster>
-      <GMapMarker
-          :key="index"
-          v-for="(m, index) in markers"
-          :position="m.position"
-          :clickable="true"
-          :draggable="true"
-          @click="center=m.position"
-      />
-    </GMapCluster>
     </GMapMap>
   </template>
   
@@ -24,13 +14,6 @@
     data() {
       return {
         center: {lat: 38.99093136536435, lng: -1.8468565086554327},
-        markers: [
-        {
-          position: {
-            lat: 38.99093136536435, lng: -1.8468565086554327
-          },
-        }
-    ]
       }
     }
   }
